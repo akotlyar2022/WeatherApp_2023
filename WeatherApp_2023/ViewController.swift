48307b223a322ad48edc63ad5f6d8eae50cbdc79
AleksandrKotliar <sashagsmnew@gmail.com>
2023-05-12 22:13:10 +0400

    Initial Commit

---
+++ WeatherApp_2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp_2023
+//
+//  Created by macbook on 12.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
